208988c63328faf40d57772ca5a6c7a59b0aa22f
mstop4 <mstop4@gmail.com>
2018-07-11 18:20:31 -0400

    transitiion end

--- Day 1/day1.js
+++ Day 1/day1.js
@@ -1,6 +1,25 @@
 window.addEventListener("keydown", function(e) {
   const audio = this.document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = this.document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
   if (!audio) 
     return; // no audio element found, give up
+
+  // rewind audio then play
+  audio.currentTime = 0; 
   audio.play();
+
+  key.classList.add('playing');
+});
+
+function removeTransition(e) {
+  //only listen for transform end
+  if (e.propertyName !== 'transform')
+    return;
+  this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(function(key) {
+  key.addEventListener('transitionend', removeTransition)
 });
